38d5e38df492f74efb09a492dafececccdc47c12
solangeguzman <solangeguzmanduchen@gmail.com>
2021-07-08 17:25:58 +0200

    codice

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+Vue.config.devtools = true;
+
+new Vue(
+ {
+    el:'#app',
+    data: {
+        emails:[]
+    },
+    created(){
+        this.getEmails();
+    },
+    methods:{
+      getEmails: function(){
+        for(let i = 0; i<10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response)=>{
+                this.emails.push(response.data.response)
+            })
+        }
+    } 
+},
+
+})
